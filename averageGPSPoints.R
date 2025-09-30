2ce4eceb1424feaf477ce55794b511e6dd8de82f
NicoJaws23 <nico.jaworski@utexas.edu>
2025-09-30 14:35:45 -0500

    startingto compare time

--- averageGPSPoints.R
+++ averageGPSPoints.R
@@ -57,3 +57,155 @@ head(output)
 ####ALWAYS CHANGE NAME OF OUTPUT FILE####
 write.table(output, file="C:/Users/Jawor/Desktop/R_repos/playbackAnalysis/averagedPoints.csv",row.names=TRUE,col.names=TRUE,sep=",")
 
+################################################################################
+################################################################################
+################################################################################
+####################Averaged gps points using ChatGPT###########################
+
+library(dplyr)
+library(lubridate)
+library(tidyverse)
+library(leaflet)
+library(geosphere)
+
+track_files <- list.files("C:\\Users\\Jawor\\Desktop\\R_repos\\playbackAnalysis\\2025Avistaje", pattern = "\\.csv$", full.names = TRUE)
+data <- lapply(track_files, read.table, sep=",", header=TRUE)
+combined.data <- do.call(rbind, data)
+combined.data$file_origin <- row.names(combined.data)
+combined.data <- combined.data |>
+  mutate(time = ymd_hms(time))
+
+time_seq <- seq(floor_date(min(combined.data$time), "15 minutes"), ceiling_date(max(combined.data$time), "15 minutes"), by = "15 mins")
+
+avg_window <- function(target_time, data, window = 2) {
+  subset <- data |>
+    filter(time >= target_time - minutes(window),
+           time <= target_time + minutes(window))
+  
+  if (nrow(subset) > 0) {
+    tibble(
+      target_time = target_time,
+      mean_lat = mean(subset$Latitude, na.rm = TRUE),
+      mean_lon = mean(subset$Longitude, na.rm = TRUE),
+      n_points = nrow(subset)
+    )
+  } else {
+    tibble(
+      target_time = target_time,
+      mean_lat = NA_real_,
+      mean_lon = NA_real_,
+      n_points = 0
+    )
+  }
+}
+
+avgPoints <- bind_rows(lapply(time_seq, avg_window, data = combined.data))
+
+avgPoints <- avgPoints |>
+  filter(!is.na(mean_lat), !is.na(mean_lon))
+
+write.csv(avgPoints, file = "C:/Users/Jawor/Desktop/R_repos/playbackAnalysis/avgPoints.csv",row.names=TRUE,col.names=TRUE,sep=",")
+
+avgPoints2 <- avgPoints |>
+  mutate(date = as.Date(target_time), time = format(target_time, format = "%H:%M:%S"))
+
+
+
+pbPoints <- read.csv(file.choose(), header = TRUE)
+pbPointsFilt <- pbPoints |>
+  filter(symbol == "Flag")
+
+playbacks <- pbPointsFilt |>
+  mutate(dateTime = ymd_hms(wpt_class, tz = "UTC"), date = as.Date(dateTime), time = format(dateTime, format = "%H:%M:%S"))
+
+playback_matches <- playbacks %>%
+  rowwise() %>%
+  mutate(
+    match = list({
+      tracks_today <- avgPoints2 %>% filter(date == as.Date(dateTime))
+      if (nrow(tracks_today) == 0) {
+        tibble(
+          closest_track_time = NA,
+          closest_lat = NA,
+          closest_lon = NA,
+          time_diff_sec = NA
+        )
+      } else {
+        idx <- which.min(abs(tracks_today$target_time - dateTime))
+        tibble(
+          closest_track_time = tracks_today$target_time[idx],
+          closest_lat = tracks_today$mean_lat[idx],
+          closest_lon = tracks_today$mean_lon[idx],
+          time_diff_sec = as.numeric(difftime(tracks_today$target_time[idx], dateTime, units = "secs"))
+        )
+      }
+    })
+  ) %>%
+  unnest(match) %>%
+  ungroup()
+names(playback_matches)
+
+leaflet() |>
+  addTiles() |>
+  addPolylines(data = avgPoints2, lng = ~mean_lon, lat = ~mean_lat, color = "blue") |>
+  addCircleMarkers(data = avgPoints2, lng = ~mean_lon, lat = ~mean_lat, radius = 3, color = "red", popup = ~paste0("Time: ", target_time)) |>
+  addCircleMarkers(data = playback_matches, lng = ~Longitude, lat = ~Latitude, radius = 5, color = "green", fillColor = "yellow", fillOpacity = 0.8, weight = 2, popup = ~paste0(ident))
+
+leaflet(pbPointsFilt) |>
+  addTiles() |>
+  addCircleMarkers(lng = ~Longitude, lat = ~Latitude, radius = 3, color = "red", popup = ~paste0(ident))
+
+
+tracks_with_dist <- avgPoints2 |>
+  arrange(date, target_time) |>
+  group_by(date) |>
+  mutate(dist_m = distHaversine(cbind(mean_lon, mean_lat), cbind(lag(mean_lon), lag(mean_lat)))) |>
+  ungroup()
+
+playback_travel <- playback_matches |>
+  rowwise() |>
+  mutate(
+    travel_1h_m = {
+      tracks_today <- tracks_with_dist |> filter(date == as.Date(dateTime))
+      t_start <- closest_track_time
+      t_end <- t_start + hours(1)
+      seg <- tracks_today |> filter(target_time >= t_start & target_time <= t_end)
+      sum(seg$dist_m, na.rm = TRUE)
+    }
+  ) |>
+  select(ident, date, time, travel_1h_m) |>
+  ungroup()
+
+avgPoints2 <- avgPoints2 |>
+mutate(date = as.Date(target_time))
+playback_days <- unique(as.Date(playback_matches$dateTime))
+control_days <- setdiff(unique(avgPoints2$date), playback_days)
+control_days <- as.Date(control_days, origin = "1970-01-01")
+
+control_travel <- playback_matches |>
+  rowwise() |>
+  mutate(
+    control_date = sample(control_days, 1), 
+    control_travel_1h_m = {
+      tracks_control <- tracks_with_dist |> filter(date == control_date)
+      t_start <- hms::as_hms(closest_track_time)
+      t_end <- t_start + 3600
+      seg <- tracks_control |> filter(hms::as_hms(target_time) >= t_start & hms::as_hms(target_time) <= t_end)
+      sum(seg$dist_m, na.rm = TRUE)
+    }
+  ) |>
+  select(ident, control_date, control_travel_1h_m) |>
+  ungroup()
+
+comparison_df <- playback_travel |>
+  left_join(control_travel, by = c("ident"))
+
+
+
+
+
+
+
+
+
+
